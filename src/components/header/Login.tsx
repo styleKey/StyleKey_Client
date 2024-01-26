8f0d01cadb72a9826d7dab757c2f707b524d7190
jinsupark4255 <jin04007@naver.com>
2024-01-26 20:22:07 +0900

    Feat: redux-toolkit을 이용해서 로그인 상태 관리
    ## 변경사항
    - package.json에 리덕스 및 리덕스 툴킷 설치
    - main.tsx에 Provider로 App 컴포넌트 감싸서 리덕스 store 넘김
    - Header.tsx와 Login.tsx에 리덕스 상태관리 연동
    - auth.tsx에 로그인 상태관리 리듀서와 액션 선언
    - index.tsx에 리덕스 store 선언
    - 컴포넌트 내의 Appstate는 타입스크립트 때문에 선언

--- src/components/header/Login.tsx
+++ src/components/header/Login.tsx
@@ -1,10 +1,43 @@
+import NLoginLogo from './images/NLoginButton.svg';
 import LoginLogo from './images/LoginButton.svg';
 import * as Li from './styles/HeaderButtons.styles';
+import { useSelector, useDispatch } from 'react-redux';
+import { AppState } from '../../store/index.tsx';
+import { authActions } from '../../store/auth.tsx';
 
 function Login() {
+  const isAuth = useSelector((state: AppState) => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+
+  const loginHandler = (event: React.MouseEvent<HTMLImageElement>) => {
+    event.preventDefault();
+
+    dispatch(authActions.login());
+  };
+
+  const logoutHandler = (event: React.MouseEvent<HTMLImageElement>) => {
+    event.preventDefault();
+
+    dispatch(authActions.logout());
+  };
+
   return (
     <Li.HeaderButtons>
-      <img src={LoginLogo} alt="Login Button" />
+      {isAuth ? (
+        <img
+          src={LoginLogo}
+          alt="Loged in Button"
+          onClick={logoutHandler}
+          style={{ width: '26px', height: '26px' }}
+        />
+      ) : (
+        <img
+          src={NLoginLogo}
+          alt="Log in Button"
+          onClick={loginHandler}
+          style={{ width: '26px', height: '26px' }}
+        />
+      )}
     </Li.HeaderButtons>
   );
 }
