fee11e49fbeb98034355467cdf5b36f1aff7211c
jinsupark4255 <jin04007@naver.com>
2024-01-29 15:35:54 +0900

    Refactor: 폴더 구조 변경
    ## 변경사항
    - components 폴더 밑에 있던 Homepage를 src 하위 pages 폴더 생성 후 그 곳에 이동
    - LoginPage 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import styled from 'styled-components';
 import GlobalStyle from './styles/GlobalStyle';
 import Header from './components/header/Header.tsx';
-import Body from './components/body/Homepage.tsx';
+import HomePage from './pages/Homepage/Homepage.tsx';
+import Homepage from './pages/Homepage/Homepage.tsx';
 
 const CenteredContainer = styled.div`
   max-width: 390px; // 최대 가로 길이 (피그마에 있는 대로)
@@ -16,8 +19,12 @@ function App() {
     <>
       <GlobalStyle />
       <CenteredContainer>
-        <Header />
-        <Body />
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Homepage />}></Route>
+          </Routes>
+        </BrowserRouter>
       </CenteredContainer>
     </>
   );
