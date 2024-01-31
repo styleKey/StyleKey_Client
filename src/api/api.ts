692ef95909e984d5a3877f0f37baa9beee2e9bbf
byeong <qudcks4885@gmail.com>
2024-01-31 18:29:05 +0900

    feat: api layer 추가
    기존 api.get , api.post ... 방식으로 method를 property로 사용하는 방법으로 작성했으나 중복되는 코드가 너무 많아,
    method도 axios config에 넣는 방식으로 바꿨습니다.

---
+++ src/api/api.ts
@@ -0,0 +1,31 @@
+import { AxiosResponse } from 'axios';
+import axiosInstance from './axios';
+
+interface Request<D, P> {
+  url: string;
+  method: string;
+  data?: D;
+  params?: P;
+}
+
+// TODO: 응답 데이터 타입 설정
+interface Response<R> {
+  /**
+   * status
+   * isSuccess
+   */
+  data: R;
+}
+
+async function request<D, P, R>({ url, method, data, params }: Request<D, P>) {
+  const response = await axiosInstance<D, AxiosResponse<Response<R>>>({
+    url,
+    method,
+    data,
+    params,
+  });
+
+  return response;
+}
+
+export default request;
