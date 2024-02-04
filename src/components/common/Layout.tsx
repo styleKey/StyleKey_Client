e9874d94b1f6316c06d6234864273c39a48e656d
byeong <qudcks4885@gmail.com>
2024-02-04 19:10:16 +0900

    feat: 공통 레이아웃 컴포넌트

---
+++ src/components/common/Layout.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const MobileLayout = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  max-width: 390px;
+  min-width: 320px;
+  margin: 0 auto;
+  padding: 0px 16px 32px 16px;
+
+  display: flex;
+  flex-direction: column;
+`;
