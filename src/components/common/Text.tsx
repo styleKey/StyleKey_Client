ad40ce545aabb44af1ea0ec78f342bb6fda67b1b
jinsupark4255 <jin04007@naver.com>
2024-02-14 22:08:32 +0900

    Design: Media query 적용
    ## 변경사항
    - iphone se, iphone 6/7/8 plus 미디어쿼리 추가

--- src/components/common/Text.tsx
+++ src/components/common/Text.tsx
@@ -4,9 +4,19 @@ export const TextRegular = styled.p<{
   $marginTop?: number;
   $marginBottom?: number;
 }>`
-  font-size: 16px;
+  font-size: 22px;
   font-weight: 300;
   text-align: center;
+  //iphone 6/7/8 plus
+  @media (max-height: 737px) {
+    margin-top: 70px;
+    font-size: 20px;
+  }
+  //iphone SE
+  @media (max-height: 668px) {
+    margin-top: 60px;
+    font-size: 18px;
+  }
 
   margin-top: ${(props) => (props.$marginTop ? props.$marginTop : 0)}px;
   margin-bottom: ${(props) =>
@@ -27,12 +37,20 @@ export const TextTitle = styled.p<{
 `;
 
 export const Caption = styled.p<{ $color?: string }>`
-  font-size: 14px;
+  font-size: 20px;
   font-weight: 200;
   color: #343434;
   text-align: center;
 
   color: ${(props) => props.color || 'black'};
+  //iphone 6/7/8 plus
+  @media (max-height: 737px) {
+    font-size: 20px;
+  }
+  //iphone SE
+  @media (max-height: 668px) {
+    font-size: 16px;
+  }
 `;
 
 // export const TextLigth =
--- src/pages/Loginpage/Login.tsx
+++ src/pages/Loginpage/Login.tsx
@@ -17,7 +17,8 @@ function Login() {
       <Caption>나만의 스타일을 찾아주는 스타일 키</Caption>
       <TextRegular $marginTop={78}>
         로그인하면 결과를 저장하고 <br />
-        자신의 style point에 어울리는 아이템 확인도 가능해요!
+        자신의 style point에 어울리는 아이템 <br />
+        확인도 가능해요!
       </TextRegular>
       <L.ButtonContainer>
         <L.SnsCaption> SNS 계정으로 로그인</L.SnsCaption>
