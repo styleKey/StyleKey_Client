2246e9de20bb43a365d5ca039d22c0d564c84f47
jinsupark4255 <jin04007@naver.com>
2024-03-13 15:49:23 +0900

    Deploy: https 변경

--- src/components/loginpage/GoogleLogin.tsx
+++ src/components/loginpage/GoogleLogin.tsx
@@ -1,6 +1,7 @@
 import * as L from './styles/Login.style';
 const GoogleLogin = () => {
   const handleGoogleLogin = async () => {
+    //백엔드 서버 바꿔야 할듯
     window.location.href = `http://localhost:9000/oauth2/authorization/google?redirect_uri=https://style-key-client.vercel.app/oauth/redirect`;
   };
   return (
