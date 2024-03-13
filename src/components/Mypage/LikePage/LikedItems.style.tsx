60ac90555707e266293429f20a4c2ffda8617b21
byeong <qudcks4885@gmail.com>
2024-03-13 17:15:14 +0900

    feat: 좋아요한 아이템 박스 내 하트 모양 UI 구현

--- src/components/Mypage/LikePage/LikedItems.style.tsx
+++ src/components/Mypage/LikePage/LikedItems.style.tsx
@@ -10,7 +10,8 @@ export const GridLayout = styled.div`
   gap: 16px 20px;
 `;
 // TODO: 이미지, 좋아요 표시 여부,
-export const Box = styled.div<{ isLike?: boolean; itemImg?: string }>`
+export const Box = styled.div<{ itemImg?: string }>`
+  position: relative;
   width: 169px;
   height: 261px;
   flex-shrink: 0;
@@ -20,13 +21,19 @@ export const Box = styled.div<{ isLike?: boolean; itemImg?: string }>`
   background-image: url(itemImg);
 
   cursor: pointer;
+`;
 
-  &::after {
-    content: '';
-    /* background-image: url(${(props) =>
-      props.isLike ? red_heart_img : heart_img};); */
-    z-index: 2;
-  }
+export const HeartBox = styled.div<{ isLike?: boolean }>`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  display: block;
+  width: 30px;
+  height: 30px;
+  background-image: ${({ isLike }) =>
+    isLike ? `url(${red_heart_img})` : `url(${heart_img})`};
+  background-size: contain;
+  z-index: 2;
 `;
 
 export const NoItemText = styled(Text)``;
