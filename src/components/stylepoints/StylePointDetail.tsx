099039221822322afea8696be0e97a15626465f4
jinsupark4255 <jin04007@naver.com>
2024-03-19 21:33:12 +0900

    Hotfix: 개행 문자 변경

--- src/components/stylepoints/StylePointDetail.tsx
+++ src/components/stylepoints/StylePointDetail.tsx
@@ -13,7 +13,7 @@ const StylePointDetail = ({ id }: StylePoint): StylePointDetails | null => {
     case 1:
       return {
         stylepoint: '유니크',
-        styledetail: '변화에 민감하고<br/>개성을 추구하는',
+        styledetail: '변화에 민감하고\n개성을 추구하는',
         details: [
           '유니크 포인트는 개개인의 다양성과 시대에 따라 변화하는 스타일을 반영하여 평범하지 않은 독보적이고 감각적인 무드를 표현하는 스타일 유형입니다.',
           '이 유형의 사람들은 패션을 통해 자신만의 독특한 개성과 창조성을 표현하는 것을 중요하게 생각합니다. 이는 패션을 예술의 한 분야로 보는 관점에서 비롯된 문화적 흐름이며, 표현의 자유를 중시하는 현대 사회의 가치관을 반영합니다.',
@@ -25,7 +25,7 @@ const StylePointDetail = ({ id }: StylePoint): StylePointDetails | null => {
     case 2:
       return {
         stylepoint: '스트릿',
-        styledetail: '서브컬쳐와<br/>힙한 무드가 공존하는',
+        styledetail: '서브컬쳐와\n힙한 무드가 공존하는',
         details: [
           '스트릿 포인트는 국내외에서 대중적으로 가장 많이 보이는 패션 중 하나입니다.',
           '말 그대로 길거리 사람들의 패션을 일컫는 말이기도 하지만 힙합, 보드문화, 올드스쿨 스타일이 가장 주된 스타일이며 도시의 생동감과 자유로움을 표현하는 패션 유형입니다.',
@@ -38,7 +38,7 @@ const StylePointDetail = ({ id }: StylePoint): StylePointDetails | null => {
       return {
         stylepoint: '모던',
         styledetail:
-          '장식적인 것 없이 깔끔하고 심플하며<br/>직선적인 실루엣을 추구하는',
+          '장식적인 것 없이 깔끔하고 심플하며\n직선적인 실루엣을 추구하는',
         details: [
           '모던 포인트는 깔끔한 핏과 실루엣 자체에 신경을 많이 쓰는 유형으로, 불필요한 장식을 배제하고 심플한 컬러를 중심으로 디자인되어 고급스러움과 세련된 이미지를 표현하는 스타일을 선호합니다.',
           '이 스타일은 현대적인 미학을 반영한 패션으로, 기능성과 심플함을 중시합니다. 대표적으로 테일러링 재킷, 슬림한 팬츠, 클래식한 슈즈 등이 이 유형에 해당합니다.',
@@ -50,7 +50,7 @@ const StylePointDetail = ({ id }: StylePoint): StylePointDetails | null => {
     case 4:
       return {
         stylepoint: '노멀',
-        styledetail: '무난하면서도<br/>센스 포인트가 가득한',
+        styledetail: '무난하면서도\n센스 포인트가 가득한',
         details: [
           '노멀 포인트는 일반적이라는 뜻의 놈(Norm)과 핵심적이라는 뜻의 코어(Core)의 합성어로, ‘평범하고 일반적인 것들이 핵심이 된다.’라는 의미를 가지고 있습니다.',
           '쉽게 말해 ‘평범하지만 센스 있는 스타일’을 가리키는 것입니다.',
@@ -64,7 +64,7 @@ const StylePointDetail = ({ id }: StylePoint): StylePointDetails | null => {
     case 5:
       return {
         stylepoint: '러블리',
-        styledetail: '사랑스러움과<br/>로맨틱이 넘쳐나는',
+        styledetail: '사랑스러움과\n로맨틱이 넘쳐나는',
         details: [
           '러블리 포인트는 여성스러우면서도 사랑스러운 이미지를 표현하는 패션을 선호합니다.',
           '이 유형은 로맨틱하고 부드러운 느낌을 좋아하는 패션 문화를 반영하며, 플라워 프린트, 레이스, 리본 등의 로맨틱한 디테일과 파스텔 톤의 컬러 팔레트를 선호합니다.',
@@ -78,7 +78,7 @@ const StylePointDetail = ({ id }: StylePoint): StylePointDetails | null => {
     case 6:
       return {
         stylepoint: '레트로',
-        styledetail: 'Y2K감성을<br/>재해석한 빈티지한',
+        styledetail: 'Y2K감성을\n재해석한 빈티지한',
         details: [
           '레트로 포인트는 90년대와 2000년대 초기의 패션 요소를 현대적으로 재해석하여, 그 시대의 특유의 분위기를 살리면서도 현대적인 패션 감각을 유지하는 것을 선호하는 유형입니다.',
           '빈티지한 프린트, 고전적인 실루엣, 레트로한 컬러 등 다양한 방식을 활용하여 고유한 매력을 강조합니다.',
@@ -90,7 +90,7 @@ const StylePointDetail = ({ id }: StylePoint): StylePointDetails | null => {
     case 7:
       return {
         stylepoint: '글램',
-        styledetail: '섹시함이 강조되는<br/>화려하고 여성스러운',
+        styledetail: '섹시함이 강조되는\n화려하고 여성스러운',
         details: [
           '글램 포인트는 글래머러스의 줄임말로 화려함, 부티, 귀티라는 의미로 화려하고 섹시한 이미지를 중심으로 하는 패션을 선호하는 유형입니다.',
           '이 유형은 고급스러운 소재나 화려한 장식, 섹시한 실루엣 등을 활용하여 강렬하고 도발적인 이미지를 표현하는 것을 선호합니다.',
@@ -102,7 +102,7 @@ const StylePointDetail = ({ id }: StylePoint): StylePointDetails | null => {
     case 8:
       return {
         stylepoint: '액티브',
-        styledetail: '일상의 경계를<br/>허무는 스포티한',
+        styledetail: '일상의 경계를\n허무는 스포티한',
         details: [
           '액티브 포인트는 편안한 스포츠웨어, 실용적인 아웃도어 아이템, 편안한 스니커즈 등을 활용하여 활동적인 라이프스타일을 표현하는 패션 유형입니다.',
           '에슬래저룩이나, 고프코어룩이 이에 포함되며, 기능성을 중시한 디자인에서 건강과 레저를 위한 밝은 색상의 디자인에 이르기까지 활동적인 이미지를 표현한 스타일입니다.',
