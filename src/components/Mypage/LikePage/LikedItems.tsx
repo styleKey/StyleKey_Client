05c3e6cf85e02041d15320b18a8b2d83babfe808
byeong <qudcks4885@gmail.com>
2024-03-25 17:46:12 +0900

    feat: 유저 좋아요 페이지 props 추가
    TODO: 카테고리 별 분류 기능
    *categorizeItems*

--- src/components/Mypage/LikePage/LikedItems.tsx
+++ src/components/Mypage/LikePage/LikedItems.tsx
@@ -1,29 +1,63 @@
 import { useState } from 'react';
 import * as LI from './LikedItems.style';
+import useGetUserLiked from '../hooks/useGetUserLikeItem';
+import {
+  UserLikeItemType,
+  UserLikeCoordiType,
+} from '../hooks/useGetUserLikeItem';
 
-const TEMP = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+interface LikedItemsProps {
+  section: string;
+  categories: string;
+}
 
-function LikedItems() {
+// category 별 sort 기능 필요
+function LikedItems({ section, categories }: LikedItemsProps) {
   const [isLike, setIsLike] = useState(true);
+  const items = useGetUserLiked(section); // 인자로 section value
+
+  // TODO :
+  // 코디룩인 경우 style_id로 분류
+  // 아이템인 경우 category_id로 분류
+  const categorizeItems = (section: string, categories: string) => {};
 
   return (
     <LI.GridLayout>
-      {TEMP.map((item) => (
-        <LI.Box>
-          {item}
-          <LI.HeartBox
-            isLike={isLike}
-            onClick={() => setIsLike((prev) => !prev)}
-          />
-        </LI.Box>
-      ))}
-      {TEMP.length === 0 && (
+      {section === '아이템' &&
+        items?.map((item) => {
+          item = item as UserLikeItemType;
+          return (
+            <LI.Box itemImgSrc={item.item_image_url}>
+              {item.title}
+              <LI.HeartBox
+                isLike={isLike}
+                onClick={() => setIsLike((prev) => !prev)}
+              />
+            </LI.Box>
+          );
+        })}
+
+      {section === '코디룩' &&
+        items?.map((item) => {
+          item = item as UserLikeCoordiType;
+          return (
+            <LI.Box itemImgSrc={item.coordinate_look_image_url}>
+              {item.title}
+              <LI.HeartBox
+                isLike={isLike}
+                onClick={() => setIsLike((prev) => !prev)}
+              />
+            </LI.Box>
+          );
+        })}
+
+      {/* 빈 배열일 경우 */}
+      {items?.length === 0 && (
         <LI.NoItemText>
           좋아하는 코디룩이나 아이템을
           <br /> 추가해 한눈에 확인해보세요.
         </LI.NoItemText>
       )}
-      {/* 빈 배열일 경우 */}
     </LI.GridLayout>
   );
 }
