3cee5f02889b539559c3f3dd77add5f15c52e36f
Jinsu Park <jin04007@naver.com>
2024-03-25 11:50:10 +0900

    Revert "Refactor: 이미지 로딩 최적화"

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,10 +1,10 @@
 import React, { useRef } from 'react';
 import * as Ho from '../components/HomePage/styles/Homepage.styles';
 import MobileLayout from '../components/common/Layout';
-import stylekeylogo from '../components/loginpage/images/stylekeylogo.svg';
+
 import DownButton from '../components/HomePage/images/Downbutton.svg';
 import Slide from '../components/swiper/Swiper';
-import { useEffect } from 'react';
+
 import { useNavigate } from 'react-router-dom';
 
 const Homepage: React.FC = () => {
@@ -13,10 +13,6 @@ const Homepage: React.FC = () => {
   const navigateToTestPage = () => {
     navigate('/teststart');
   };
-  useEffect(() => {
-    const img = new Image();
-    img.src = stylekeylogo;
-  }, []);
   const testAccessToken = localStorage.getItem('accessToken');
   console.log('accessToken:', testAccessToken);
   const homeBottomRef = useRef<HTMLDivElement>(null);
